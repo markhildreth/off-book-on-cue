da21049b1f930470f30627644372abece0f8adc0
Mark Hildreth <mark.k.hildreth@gmail.com>
2020-01-17 12:12:12 -0500

    Broke out header component

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,13 +1,8 @@
 <script>
+	import Header from './Header.svelte';
 </script>
 
-<header class="fixed h-16 w-full top-0 bg-blue-600 text-gray-100">
-	<div class="flex mx-4 h-full justify-between items-center">
-		<h1 class="text-lg">Learn You Some Lines</h1>
-	</div>
-</header>
-<!-- Header Offset -->
-<div class="h-16"></div>
+<Header />
 
 <div class="font-inter flex flex-grow h-screen justify-center items-center">
 	<div class="text-center">
---
+++ src/components/Header.svelte
@@ -0,0 +1,7 @@
+<header class="fixed h-16 w-full top-0 bg-blue-600 text-gray-100">
+	<div class="flex mx-4 h-full justify-between items-center">
+		<h1 class="text-lg">Learn You Some Lines</h1>
+	</div>
+</header>
+<!-- Header Offset -->
+<div class="h-16"></div>
