5a8704dc4b8251d49e046981906e4b7d7dbac4d5
Mark Hildreth <mark.k.hildreth@gmail.com>
2020-01-17 12:09:25 -0500

    Moved component, start to development.

---
+++ src/components/App.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<header class="fixed h-16 w-full top-0 bg-blue-600 text-gray-100">
+	<div class="flex mx-4 h-full justify-between items-center">
+		<h1 class="text-lg">Learn You Some Lines</h1>
+	</div>
+</header>
+<!-- Header Offset -->
+<div class="h-16"></div>
+
+<div class="font-inter flex flex-grow h-screen justify-center items-center">
+	<div class="text-center">
+		<h1 class="text-5xl">
+			Hello, World!
+		</h1>
+	</div>
+</div>
