24cf5eb5926a411b6c5e89fd7eb6895f2f6264fc
Mark Hildreth <mark.k.hildreth@gmail.com>
2020-01-17 14:26:44 -0500

    Tweaked landing page.

--- src/components/Landing.svelte
+++ src/components/Landing.svelte
@@ -3,16 +3,20 @@
 </script>
 
 <div class="flex flex-col justify-evenly items-center h-full">
-	<div class="w-32 h-32 border">
-		<!-- TODO: Image -->
+	<div class="flex flex-col items-center justify-evenly h-full">
+		<div class="w-32 h-32 border text-center">
+			<!-- TODO: Image -->
+		</div>
+		<p class="text-2xl text-center">
+		"Learn You Some Lines" helps you memorize your lines.
+		</p>
+		<div class="flex flex-col">
+			<button class="button mb-8" on:click={push('/how')}>
+				How It Works
+			</button>
+			<button class="button">
+				Get Started
+			</button>
+		</div>
 	</div>
-	<p class="text-2xl text-center">
-	"Learn You Some Lines" helps you memorize your lines.
-	</p>
-	<button class="button m-4" on:click={push('/how')}>
-		How It Works
-	</button>
-	<button class="button">
-		Get Started
-	</button>
 </div>
