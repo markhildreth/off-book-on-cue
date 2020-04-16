81b13d6c85803a23ca63f0891712458901858adc
Mark Hildreth <mark.k.hildreth@gmail.com>
2020-04-16 10:46:32 -0400

    Show up arrow SVG in playback bar at bottom of screen.

--- src/components/PlaybackBar.svelte
+++ src/components/PlaybackBar.svelte
@@ -21,6 +21,7 @@
 
 <div on:click={openFullPlayback} class="flex h-full w-full justify-between items-center px-2 cursor-pointer">
 	<p>{$playlist.scene.name}</p>
+	<svg class="h-4 self-start mt-2 text-gray-400 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8"><path d="M4 1L0 5l1.5 1.5L4 4l2.5 2.5L8 5 4 1z"/></svg>
 	{#if $playback.isPlaying}
 		<div class="h-full" on:click|stopPropagation={onPause}>
 			<Pause class="h-full"/>
