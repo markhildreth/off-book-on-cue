488f34f5a3f4ec8d957eab34d2b75cdb498f2a18
Mark Hildreth <mark.k.hildreth@gmail.com>
2020-04-16 11:17:28 -0400

    Added arrow down. Better centering on arrow up

--- src/components/Playback.svelte
+++ src/components/Playback.svelte
@@ -16,8 +16,11 @@
 </style>
 
 <div class="w-full h-full flex flex-col">
-	<div on:click={shrink} class="w-full flex justify-center items-center h-16 cursor-pointer border-b" style="min-height: 4rem;">
-		<h2 class="text-center">{$playlist.scene.name}</h2>
+	<div on:click={shrink} class="relative w-full flex justify-center items-center h-16 cursor-pointer border-b" style="min-height: 4rem;">
+		<h2>{$playlist.scene.name}</h2>
+		<svg class="pb-1 absolute w-4 bottom-0 text-gray-400 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8">
+			<path d="M1.5 1L0 2.5l4 4 4-4L6.5 1 4 3.5 1.5 1z"/>
+		</svg>
 	</div>
 	<div class="flex-grow overflow-scroll">
 		<div class="flex flex-col container m-auto px-4 h-full">
--- src/components/PlaybackBar.svelte
+++ src/components/PlaybackBar.svelte
@@ -19,9 +19,16 @@
 	}
 </script>
 
-<div on:click={openFullPlayback} class="flex h-full w-full justify-between items-center px-2 cursor-pointer">
-	<p>{$playlist.scene.name}</p>
-	<svg class="h-4 self-start mt-2 text-gray-400 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8"><path d="M4 1L0 5l1.5 1.5L4 4l2.5 2.5L8 5 4 1z"/></svg>
+<div on:click={openFullPlayback} class="flex h-full w-full justify-between px-2 cursor-pointer">
+	<div class="flex-1 flex items-center">
+		<p>{$playlist.scene.name}</p>
+	</div>
+	<div class="flex-1 flex justify-center">
+		<svg class="h-4 self-start mt-2 text-gray-400 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8">
+			<path d="M4 1L0 5l1.5 1.5L4 4l2.5 2.5L8 5 4 1z"/>
+		</svg>
+	</div>
+	<div class="flex-1 flex justify-end h-full">
 	{#if $playback.isPlaying}
 		<div class="h-full" on:click|stopPropagation={onPause}>
 			<Pause class="h-full"/>
@@ -31,5 +38,6 @@
 			<Play class="h-full" />
 		</div>
 	{/if}
+	</div>
 </div>
 
