84630cc5847c7bea47bf704a463601dbfdf92d91
Mark Hildreth <mark.k.hildreth@gmail.com>
2020-04-16 14:12:20 -0400

    Massively simplified layout to best account for height/address bar problems.

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -19,45 +19,46 @@
 	$: showPlaybackFooter = $currentScene != null && screen !== "playback";
 </script>
 
-{#if screen !== "loading"}
-<header class="z-50 fixed h-16 w-full top-0">
-	<Header />
-</header>
-{/if}
+<div class="flex flex-col h-full max-w-lg">
+	{#if screen !== "loading"}
+	<header class="h-16">
+		<Header />
+	</header>
+	{/if}
 
-<div class="h-full w-full absolute pt-16" class:pb-16={showPlaybackFooter}>
-	{#if screen === "loading"}
-	<Loading />
-	{:else if screen === "landing"}
-	<Landing />
-	{:else if screen === "delete_plays"}
-	<DeletePlays deletePlayId={$appState.deletePlayId} />
-	{:else if screen === "how"}
-	<How />
-	{:else if screen === "info"}
-	<Info />
-	{:else if screen === "new_play"}
-	<NewPlay />
-	{:else if screen === "plays"}
-	<Plays />
-	{:else if screen === "play"}
-	<Play playId={$appState.playId} deleteSceneId={$appState.deleteSceneId} />
-	{:else if screen === "record"}
-	<Record playId={$appState.playId} />
-	{:else if screen === "playback" }
-	<Playback />
-	{:else if screen === "edit_scene" }
-	<EditScene sceneId={$appState.sceneId} />
-	{:else if screen === "error" }
-	<Error message={$appState.message} />
-	{:else}
-	<Error message={`Unknown screen "${screen}"`} />
+	<div class="flex-1 overflow-scroll">
+		{#if screen === "loading"}
+		<Loading />
+		{:else if screen === "landing"}
+		<Landing />
+		{:else if screen === "delete_plays"}
+		<DeletePlays deletePlayId={$appState.deletePlayId} />
+		{:else if screen === "how"}
+		<How />
+		{:else if screen === "info"}
+		<Info />
+		{:else if screen === "new_play"}
+		<NewPlay />
+		{:else if screen === "plays"}
+		<Plays />
+		{:else if screen === "play"}
+		<Play playId={$appState.playId} deleteSceneId={$appState.deleteSceneId} />
+		{:else if screen === "record"}
+		<Record playId={$appState.playId} />
+		{:else if screen === "playback" }
+		<Playback />
+		{:else if screen === "edit_scene" }
+		<EditScene sceneId={$appState.sceneId} />
+		{:else if screen === "error" }
+		<Error message={$appState.message} />
+		{:else}
+		<Error message={`Unknown screen "${screen}"`} />
+		{/if}
+	</div>
+
+	{#if showPlaybackFooter}
+	<footer class="top-shadow h-16">
+		<PlaybackBar />
+	</footer>
 	{/if}
 </div>
-
-{#if showPlaybackFooter}
-<footer class="fixed h-16 w-full bottom-0 top-shadow">
-	<PlaybackBar />
-</footer>
-<div class="h-16"></div>
-{/if}
