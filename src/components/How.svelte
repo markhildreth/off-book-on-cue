f158abfb304c4213ef76d0c4e302162d673b167a
Mark Hildreth <mark.k.hildreth@gmail.com>
2020-04-16 04:17:29 -0400

    Added "How" page

--- src/components/How.svelte
+++ src/components/How.svelte
@@ -1,6 +1,20 @@
 <script>
 </script>
 
-<div class="flex flex-col justify-center h-full">
-	<h1 class="text-center text-2xl">TODO</h1>
+<div class="w-full px-4 overflow-y-auto h-full">
+	<h1 class="mt-4 text-center text-2xl">How it Works</h1>
+	<p class="mb-2">"Learn You Some Lines" works by taking you through the following steps:</p>
+	<h2 class="font-semibold text-xl">1.) Record Scenes</h2>
+	<p class="mb-2">Using your microphone with the script open in front of you, record the lines of all characters, <em>including</em> your own. While recording your lines, hold down the "My Line" button.</p>
+	<h2 class="font-semibold text-xl">2.) Playback Scenes</h2>
+	<p class="mb-2">Playback the recordings that you made, setting the volume of your own lines. If you can look at the script, mute your own lines and read the script if you are stuck. Or put your lines on a low volume; then you can talk over it when you know the lines and just listen when you don't!</p>
+	<h2 class="font-semibold text-xl">3.) Hit Specific Scenes</h2>
+	<p class="mb-2">When first starting out, just have a specific scene run on repeat and drill, drill, drill. Later, as you get better, you can enable the scenes to flow from one to the next to mimic a true performance.</p>
+	<h1 class="mt-2 mb-2 text-2xl">Other Tips</h1>
+	<ul class="list-disc ml-4 mb-4">
+		<li>When recording, aim for keeping your scenes two or less minutes long, breaking up longer scenes into smaller ones. When you rehearse the scene, it will benefit you when you can put a scene on repeat and go over the same lines multiple times in quick succession.</li>
+		<li>Sleep is so very important to memorizing. Make sure you are getting plenty of sleep. Also, try to rehearse a bit every day (say, a half hour, preferably before going to bed) rather than relying on rehearsing a lot one or two days a week. This gives you more hours of sleep that your brain can "lock in" those lines!</li>
+		<li>This app also has a slider that will increase the speed of playback. Especially if you are performing in a show where there are fast-paced lines, rehearsing at faster than the speed at which you would normally perform the scene is a great exercise to help you find gaps in your recall.</li>
+		<li>Make sure that you go back every once and awhile and check that the lines you have memorized are actually the right lines! Remember, practice doesn't make perfect, it makes permanent!</li>
+	</ul>
 </div>
